96d3b28d819eb454de49989da5e2bc99f81a687f
samed <samed_semi@live.nl>
2019-05-15 14:48:20 +0200

    1.0

--- src/com/databaseproject/MasterclassScherm.java
+++ src/com/databaseproject/MasterclassScherm.java
@@ -7,8 +7,17 @@ import java.awt.event.ActionListener;
 
 public class MasterclassScherm extends JFrame implements ActionListener {
 
+    private JButton masterclassAanmaken = new JButton("Masterclass aanmaken");
+    private JButton gastInscrhijvenMasterclass = new JButton("Gast inscrhijven");
+
+
+    String data[][] = {{"101", "Jan", "100", ""}, {"102", "Piet", "200", ""}, {"101", " ", " ", ""}};
+    String column[] = {"Datum", "Naam", "Inleggeld", "Rating"};
+
+    private JTable tabel = new JTable(data, column);
+
     public MasterclassScherm() throws HeadlessException {
-        setTitle("FullHouse - Toernooi");
+        setTitle("FullHouse - MasterClass");
         setResizable(true);
         setSize(new Dimension(800, 600));
         MasterclassComponent();
@@ -16,10 +25,36 @@ public class MasterclassScherm extends JFrame implements ActionListener {
 
     private void MasterclassComponent() {
 
+        JPanel panel = new JPanel();
+        panel.add(masterclassAanmaken);
+        masterclassAanmaken.addActionListener(this);
+        panel.add(gastInscrhijvenMasterclass);
+        gastInscrhijvenMasterclass.addActionListener(this);
+        add(new JScrollPane(tabel));
+        panel.add(tabel.getTableHeader());
+        GridLayout grid = new GridLayout(4, 4, 30, 20);
+        Container content = this.getContentPane();
+        content.setLayout(grid);
+        add(panel);
+        pack();
+
+
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        if (e.getSource() == gastInscrhijvenMasterclass) {
+            JDialog gastInschrijvenPopup = new JDialog();
+            gastInschrijvenPopup.setSize(500, 500);
+            gastInschrijvenPopup.setVisible(true);
+        }
+        if (e.getSource() == masterclassAanmaken) {
+            JDialog masterclassAanmakenPopup = new JDialog();
+            masterclassAanmakenPopup.setSize(500, 500);
+            masterclassAanmakenPopup.setVisible(true);
+        }
 
     }
 }
--- src/com/databaseproject/ToernooiScherm.java
+++ src/com/databaseproject/ToernooiScherm.java
@@ -7,18 +7,88 @@ import java.awt.event.ActionListener;
 
 public class ToernooiScherm extends JFrame implements ActionListener {
 
+    private JButton toernooiAanmaken = new JButton("Toernooi aanmaken");
+    private JButton gastInscrhijven = new JButton("Gast inscrhijven");
+    private JButton ratingBijwerken = new JButton("Rating bijwerken");
+    private JButton winnaarRegistreren = new JButton("Winnaar regsitreren");
+    private JButton inleggeldBijwerken = new JButton("Inleggeld bijwerken");
+    private JButton tafelIndelen = new JButton("Tafel indelen");
+
+    String data[][] = {{"101", "Jan", "100", ""}, {"102", "Piet", "200", ""}, {"101", " ", " ", ""}};
+    String column[] = {"Datum", "Naam", "Inleggeld", "Rating"};
+
+    private JTable tabel = new JTable(data, column);
+
     public ToernooiScherm() throws HeadlessException {
         setTitle("FullHouse - Toernooi");
         setResizable(true);
-        setSize(new Dimension(800, 600));
+        setSize(new Dimension(800, 800));
         ToernooiComponent();
     }
 
+
     private void ToernooiComponent() {
+        JPanel panel = new JPanel();
+        panel.add(toernooiAanmaken);
+        toernooiAanmaken.addActionListener(this);
+        panel.add(gastInscrhijven);
+        gastInscrhijven.addActionListener(this);
+        panel.add(ratingBijwerken);
+        ratingBijwerken.addActionListener(this);
+        panel.add(winnaarRegistreren);
+        winnaarRegistreren.addActionListener(this);
+        panel.add(inleggeldBijwerken);
+        inleggeldBijwerken.addActionListener(this);
+        panel.add(tafelIndelen);
+        tafelIndelen.addActionListener(this);
+        add(new JScrollPane(tabel));
+        panel.add(tabel.getTableHeader());
+        GridLayout grid = new GridLayout(4, 4, 30, 20);
+        Container content = this.getContentPane();
+        content.setLayout(grid);
+        add(panel);
+        pack();
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == toernooiAanmaken) {
+            ToernooiScherm scherm = new ToernooiScherm();
+            scherm.setLocationRelativeTo(scherm);
+            scherm.setVisible(true);
+        }
+        if (e.getSource() == gastInscrhijven) {
+            JDialog gastInschrijvenPopup = new JDialog();
+            gastInschrijvenPopup.setSize(500, 500);
+            gastInschrijvenPopup.setVisible(true);
+        }
+        if (e.getSource() == ratingBijwerken) {
+            JDialog ratingbijwerkenPopup = new JDialog();
+            ratingbijwerkenPopup.setSize(500, 500);
+            ratingbijwerkenPopup.setVisible(true);
+        }
+        if (e.getSource() == winnaarRegistreren) {
+            JDialog winnaarRegistrerenPopup = new JDialog();
+            winnaarRegistrerenPopup.setSize(500, 500);
+            winnaarRegistrerenPopup.setVisible(true);
+        }
+        if (e.getSource() == inleggeldBijwerken) {
+            JDialog inleggeldBijwerkenPopup = new JDialog();
+            inleggeldBijwerkenPopup.setSize(500, 500);
+            inleggeldBijwerkenPopup.setVisible(true);
+
+        }
+        if (e.getSource() == tafelIndelen) {
+            JDialog tafelIndelenPopup = new JDialog();
+            tafelIndelenPopup.setSize(500, 500);
+            tafelIndelenPopup.setVisible(true);
+
+        }
+
 
     }
+
+
 }
+
